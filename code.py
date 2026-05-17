a2373fb9e4278153f75a18d8c53b01a227702306
ABDUL AFNAN <abdulafnan405@gmail.com>
2026-05-17 20:31:09 +0530

    Update code

---
+++ code.py
@@ -0,0 +1,7 @@
+population = 10000
+pop =10000
+for i in range (2011,2021):
+  pop = 0.1*pop
+  population = population+0.1*population
+  print(i,"me itne loga bade-->",pop,"!""Total->",population)
+  
