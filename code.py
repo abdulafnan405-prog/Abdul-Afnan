16d51b797a31ebe78f06167591fc32e8fb4aea89
ABDUL AFNAN <abdulafnan405@gmail.com>
2026-05-19 00:25:44 +0530

    Updated file

--- code.py
+++ code.py
@@ -1,7 +1,17 @@
-population = 10000
-pop =10000
-for i in range (2011,2021):
-  pop = 0.1*pop
-  population = population+0.1*population
-  print(i,"me itne loga bade-->",pop,"!""Total->",population)
+# population = 10000
+# pop =10000
+# for i in range (2011,2021):
+#   pop = 0.1*pop12
+#   population = population+0.1*population
+#   print(i,"me itne loga bade-->",pop,"!""Total->",population)
   
+
+lower = int(input('enter lower range'))
+upper = int(input('enter upper range'))
+
+for i in range(lower,upper+1):
+  for j in range(2,i):
+    if i%j == 0:
+      break
+  else:
+    print(i,"prime number") 
